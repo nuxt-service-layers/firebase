9af19dc4344284b5caa8589859a325d812df4d6c
Cody Wakeford <codypwakeford@gmail.com>
2024-10-24 20:34:23 +0100

    changed connection file

--- server/plugins/firebase.ts
+++ server/plugins/firebase.ts
@@ -21,9 +21,9 @@ export default defineNitroPlugin((nitroApp) => {
 		app = initializeApp(firebaseConfig)
 		db = getFirestore(app)
 
-		console.log("[Firebase Base Layer] Firebase server side initialized.")
+		console.log("\x1b[32m", "[database-connection] Firebase Connected.", "\x1b[0m")
 	} catch (error) {
-		console.error("[Firebase Base Layer] Error creating Firebase server side instance", error)
+		console.error("[database-connection] Error connecting to Firebase.", error)
 	}
 
 	// Provide the Firebase services to the Nuxt app context
